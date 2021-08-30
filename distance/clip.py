d3955cf98aabadc496db12de58c02a8092fe4770
yuenhy <16226884+yuenhy@users.noreply.github.com>
2021-08-30 06:59:05 +0100

    clip loss

---
+++ distance/clip.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import clip
+from PIL import Image
+from tqdm import tqdm
+import h5py
+import clip
+import time
+import numpy as np
+
+start_time = time.time()
+
+def measure_distance(original, generated):
+    # distance = 0
+    return F.mse_loss(original, generated)
+
+def calculate_logits(image_features, text_features):
+  logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+  # normalize
+  image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+  text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+  # cosine similarity as logits
+  logit_scale = logit_scale.exp()
+  logits_per_image = logit_scale * image_features @ text_features.t()
+  logits_per_text = logit_scale * text_features @ image_features.t()
+
+  # shape = [global_batch_size, global_batch_size]
+  return logits_per_image, logits_per_text
+
+def calculate_prob(image_features, text_features):
+  logits_per_image, logits_per_text = calculate_logits(image_features, text_features)
+  probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+  return probs
+
+def generate_clip_score(device, img, text):
+  model, preprocess = clip.load("ViT-B/32", device=device)
+  image = preprocess(Image.open(img)).unsqueeze(0).to(device)
+  with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+  probs = calculate_prob(image_features, text_features)
+  return probs
+
+def get_clip_score(device, encoded_img, text):
+  model, _ = clip.load("ViT-B/32", device=device)
+  
+  with h5py.File(str(encoded_img), 'r') as hf:
+    image_features = hf["encoding"][:]
+
+  with torch.no_grad():
+    text_features = model.encode_text(text)
+
+  probs = calculate_prob(image_features, text_features)
+  
+  return probs
+    
+
+def score_by_clip(original_imgs, generated_imgs, prompts, original_encodings=False):
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    probs = []
+
+    for i in tqdm(range(len(generated_imgs))):
+
+        model, preprocess = clip.load("ViT-B/32", device=device)
+        text = clip.tokenize(prompts[i]).to(device)
+        prob = 0
+        if original_encodings:
+          prob = get_clip_score(device, original_encodings[i], text).tolist()[0]
+        else:
+          prob = generate_clip_score(device, generated_imgs[i], text).tolist()[0]
+
+
+        probs.append(prob)
+
+    return probs
